e1c1c927b34e7f532cf6ba9f1b666182967fcb37
VMB <peter200970@hotmail.com>
2021-11-23 00:16:26 -0500

    Angular refresh

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,34 @@
+import {Task} from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: "Doctors Appointment",
+    day: 'May 5th at 2:30pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: "Meeting at school",
+    day: 'May 6th at 2:30pm',
+    reminder: false
+  },
+  {
+    id: 3,
+    text: "Food shopping",
+    day: 'May 7th at 2:30pm',
+    reminder: false
+  },
+  {
+    id: 4,
+    text: "Bar meeting with friends",
+    day: 'May 8th at 2:30pm',
+    reminder: true
+  },
+  {
+    id: 5,
+    text: "Doctors Appointment",
+    day: 'May 9th at 2:30pm',
+    reminder: false
+  },
+]
